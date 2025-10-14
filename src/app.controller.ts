2063c2db823bb40cedca6ab4fdb260807b8d8f28
filipexyz <luis@filipe.xyz>
2025-10-14 00:53:43 +0000

    fix: use import type for Response to fix TypeScript isolated modules error

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,5 @@
 import { Controller, Get, Res } from '@nestjs/common';
-import { Response } from 'express';
+import type { Response } from 'express';
 import { AppService } from './app.service';
 import { join } from 'path';
 
